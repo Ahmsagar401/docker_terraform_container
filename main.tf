0e39c50291632b77b2236cd65be20f178fdf18d5
Ahmsagar401 <154943092+Ahmsagar401@users.noreply.github.com>
2024-03-10 19:13:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Security group start here
+
+resource "aws_security_group" "allow_SG" {
+  name        = "my_SG"
+  description = "ssh"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+# Security group end here
+
+resource "aws_instance" "example" {
+  ami                    = "ami-07d9b9ddc6cd8dd30"
+  instance_type          = "t2.micro"
+  security_groups        = [aws_security_group.allow_SG.name]
+  tags = {
+    Name = "docker-server"
+  }
+}
